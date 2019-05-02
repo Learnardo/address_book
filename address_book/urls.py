d5da8be96da63060422a5a5679ecde97693559b2
Guillaume Parrou <guillaumeparrou@gmail.com>
2019-05-02 21:03:34 +0200

    contact management

--- address_book/urls.py
+++ address_book/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('add_address', views.add_address, name="add_address"),
+    path('edit/<address_id>', views.edit_address, name="edit_address"),
+    path('delete/<address_id>', views.delete_address, name="delete_address"),
 ]
--- address_book/views.py
+++ address_book/views.py
@@ -1,8 +1,44 @@
 from django.shortcuts import render, redirect
+from .models import Address
+from .forms import AddressForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    return render(request, 'address_book/home.html',{})
+    all_addresses = Address.objects.all()
+    return render(request, 'address_book/home.html',{'all_addresses':all_addresses})
 
 def add_address(request):
-    return render(request, 'address_book/add_address.html',{})
+    if request.method == 'POST':
+        form = AddressForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            messages.success(request, ('Address has been added!'))
+            return redirect('home')
+        else:
+            messages.success(request, ('Seems like there was an error...'))
+            return render(request, 'address_book/add_address.html', {})
+
+    else:
+        return render(request, 'address_book/add_address.html', {})
+
+def edit_address(request, address_id):
+    current_address = Address.objects.get(pk=address_id)
+    if request.method == 'POST':
+        form = AddressForm(request.POST or None, instance=current_address)
+        if form.is_valid():
+            form.save()
+            messages.success(request, ('Address has been added!'))
+            return redirect('home')
+        else:
+            messages.success(request, ('Seems like there was an error...'))
+            return render(request, 'address_book/add_address.html', {})
+    else:
+        return render(request, 'address_book/edit.html', {'current_address':current_address})
+
+def delete_address(request, address_id):
+    current_address = Address.objects.get(pk=address_id)
+    current_address.delete()
+    messages.success(request, ('Address has been deleted!'))
+
+    return redirect('home')
